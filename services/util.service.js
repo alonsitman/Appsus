3b465927c0ec3e95d8a364441aba3250aee22ec5
yuvalsidis <yuvalsidis@gmail.com>
2024-03-13 12:59:33 +0200

    Add storageService to utilServiceJs

--- services/util.service.js
+++ services/util.service.js
@@ -60,3 +60,12 @@ function getMonthName(date) {
     ]
     return monthNames[date.getMonth()]
 }
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+    var val = localStorage.getItem(key)
+    return JSON.parse(val)
+}
