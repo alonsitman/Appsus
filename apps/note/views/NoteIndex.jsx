c1fae6bd1ce276f566a74a08d669eda0729d8e70
yuvalsidis <yuvalsidis@gmail.com>
2024-03-13 18:00:58 +0200

    Modify NoteIndex loadnotes function

--- apps/note/views/NoteIndex.jsx
+++ apps/note/views/NoteIndex.jsx
@@ -1,5 +1,29 @@
+const { useState, useEffect } = React
 import { noteService } from "../services/note.service.js"
+import { NoteList } from "../cmps/NoteList.jsx"
 
 export function NoteIndex() {
-    return <div>note app</div>
+    const [notes, setNotes] = useState(null)
+
+    useEffect(()=>{
+        loadNotes()
+    },[])
+
+    function loadNotes() {
+        noteService.query()
+        .then((notes) =>{
+            setNotes(notes)
+            console.log('need to replace it for succsess messege etc..')
+        })
+        .catch((err) =>{
+            console.log('Error occur by loading notes', err)
+        })
+    }
+
+    
+    if(!notes) return <React.Fragment>loading...</React.Fragment>
+    return <section className="note-index">
+            <h1>notes</h1>
+            <NoteList notes={notes}/>
+        </section>
 }
