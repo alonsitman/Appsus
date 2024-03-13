08cf6444fd6ba88cd5e0f12db2bac6a70ad8fa49
alon.sitman <alonsitman@gmail.com>
2024-03-13 15:27:26 +0200

    Add remove mail

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -1,11 +1,14 @@
 import { mailService } from "../services/mail.service.js"
 import { MailPreview } from "./MailPreview.jsx"
 
-export function MailList({mails}) {
+export function MailList({mails, onRemoveMail}) {
     return <ul className="mail-list">
         {
             mails.map(mail => <li key={mail.id}>
                 <MailPreview mail={mail} />
+                <div className="mail-actions">
+                    <button className="remove-btn" onClick={() => onRemoveMail(mail.id)}>X</button>
+                </div>
             </li>)
         }
 
--- apps/mail/views/MailIndex.jsx
+++ apps/mail/views/MailIndex.jsx
@@ -17,11 +17,26 @@ export function MailIndex() {
             })
     }
 
+    function onRemoveMail(mailId) {
+        console.log('mailId:', mailId)
+        mailService.remove(mailId)
+            .then(() => {
+                console.log('removed')
+                setMails((prevMails) => prevMails.filter(mail => mail.id !== mailId))
+            })
+            .catch((err) => {
+                console.log('had issues removing mail', err)
+            })
+    }
+
     console.log('mails from mail index:', mails)
     if (!mails) return <div>Loading...</div>
     return <section className="mail-index">
         <h1>mail app</h1>
-        <MailList mails={mails}/>
+        <MailList
+            mails={mails}
+            onRemoveMail={onRemoveMail}
+        />
       
      
     </section>
