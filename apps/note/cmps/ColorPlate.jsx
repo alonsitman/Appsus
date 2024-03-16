09f91fd76674cf0a3671a322a9765028e6d5c270
yuvalsidis <yuvalsidis@gmail.com>
2024-03-16 11:30:39 +0200

    Fix ColorPlate position and style

--- apps/note/cmps/ColorPlate.jsx
+++ apps/note/cmps/ColorPlate.jsx
@@ -1,9 +1,9 @@
 import { noteService } from "../services/note.service.js"
 
-export function ColorPlate(){
+export function ColorPlate() {
     return <section className="color-plate">
         {
-            noteService.getColorPicker().map(color => <button id={`'color-${color}`}></button>)
+            noteService.getColorPicker().map(color => <button className="btn" id={`'color-${color}`} style={{ backgroundColor: color }}></button>)
         }
     </section>
 }
