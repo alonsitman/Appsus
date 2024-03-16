304870efd0a2aafaebc6f70d4227cbbfa5af60a4
yuvalsidis <yuvalsidis@gmail.com>
2024-03-16 16:05:37 +0200

    Add NoteFilterBarLabels and link

---
+++ apps/note/cmps/NoteFilterBarLabels.jsx
@@ -0,0 +1,7 @@
+
+export function NoteFilterBarLabels() {
+    return <section class="labels">
+        <button class=" btn label-btn"></button>
+        <p>Personal</p>
+    </section>
+}
--- apps/note/cmps/NoteFilterBarPreview.jsx
+++ apps/note/cmps/NoteFilterBarPreview.jsx
@@ -1,6 +1,22 @@
-
+import { NoteFilterBarLabels } from "./NoteFilterBarLabels.jsx"
 export function NoteFilterBarPreview() {
     return <section>
-        <div>hi i work now</div>
+        <section class="notes-main">
+            <button class=" btn notes-filter-btn"></button>
+            <p>Notes</p>
+        </section>
+              <NoteFilterBarLabels/>
+        <section class="edit-labels">
+            <button class=" btn edit-labels-btn"></button>
+            <p>Edit labels</p>
+        </section>
+        <section class="archive">
+            <button class=" btn archive-btn"></button>
+            <p>Archive</p>
+        </section>
+        <section class="trash">
+            <button class=" btn trash"></button>
+            <p>Trash</p>
+        </section>
     </section>
 }
