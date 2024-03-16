65416bf7aa1be93188858cde059d25e947bd02e2
yuvalsidis <yuvalsidis@gmail.com>
2024-03-16 11:51:08 +0200

    Change color plate style css

--- apps/note/cmps/NotePreviewToolBar.jsx
+++ apps/note/cmps/NotePreviewToolBar.jsx
@@ -1,15 +1,15 @@
 import { ColorPlate } from "./ColorPlate.jsx"
 
 
-export function NotePreviewToolBar({note, onRemoveNote}) {
-    
+export function NotePreviewToolBar({ note, onRemoveNote }) {
+
     return <section className="note-preview-tool-bar">
         <button className="btn delete-btn trashIcon" onClick={() => onRemoveNote(note.id)}></button>
+        <button className="btn background-color-btn fa-solid fa-palette">
+            <ColorPlate />
+        </button>
         <button className="btn archive-btn archiveIcon"></button>
         <button className="btn add-image-btn imageIcon"></button>
-        <button className="btn background-color-btn fa-solid fa-palette">
-           <ColorPlate/>
-        </button>
         <button className="btn duplicate-btn duplicateIcon"></button>
     </section>
 }
