4e34eeec0451d3b690bd27a87c1d70dc3488ae44
yuvalsidis <yuvalsidis@gmail.com>
2024-03-16 18:22:57 +0200

    Final init of note

--- apps/note/cmps/NoteFilterBarPreview.jsx
+++ apps/note/cmps/NoteFilterBarPreview.jsx
@@ -1,22 +1,44 @@
-import { NoteFilterBarLabels } from "./NoteFilterBarLabels.jsx"
+const { useState } = React;
+import { NoteFilterBarLabels } from "./NoteFilterBarLabels.jsx";
+
 export function NoteFilterBarPreview() {
-    return <section>
-        <section class="notes-main">
-            <button class=" btn notes-filter-btn"></button>
-            <p>Notes</p>
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    return (
+        <section 
+            className="note-filter-bar-preview"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+        >
+            <section className="bar-section notes-main">
+                <button className="btn notes-filter-btn noteIcon"></button>
+                {isHovered && <p>Notes</p>}
+            </section>
+            <section className="bar-section reminder">
+                <button className="btn reminder-btn reminderIcon"></button>
+                {isHovered && <p>Reminder</p>}
+            </section>
+            <NoteFilterBarLabels isHovered={isHovered} />
+            <section className="bar-section edit-labels">
+                <button className="btn edit-labels-btn editLabelsIcon"></button>
+                {isHovered && <p>Edit labels</p>}
+            </section>
+            <section className="bar-section archive">
+                <button className="btn archive-btn archiveIcon"></button>
+                {isHovered && <p>Archive</p>}
+            </section>
+            <section className="bar-section trash">
+                <button className="btn trashIcon"></button>
+                {isHovered && <p>Trash</p>}
+            </section>
         </section>
-              <NoteFilterBarLabels/>
-        <section class="edit-labels">
-            <button class=" btn edit-labels-btn"></button>
-            <p>Edit labels</p>
-        </section>
-        <section class="archive">
-            <button class=" btn archive-btn"></button>
-            <p>Archive</p>
-        </section>
-        <section class="trash">
-            <button class=" btn trash"></button>
-            <p>Trash</p>
-        </section>
-    </section>
+    );
 }
